f93b39724341687e847d45b9e3dbf32f91d2a298
Aiden Jackson <142336299+aiduJ@users.noreply.github.com>
2026-04-25 01:35:52 -0500

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1) {
+            return 1;
+        }
+        if(n==2) {
+            return 2;
+        }
+        int curr = 2;
+        int prev = 1;
+        for (int i=3; i<=n; ++i) {
+            int sum = prev + curr;
+            prev = curr;
+            curr = sum;
+        }
+        return curr;
+    }
+}
